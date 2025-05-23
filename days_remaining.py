e2813bc1cb6c08470ccdb8e9420f1b7cd05c2a99
dltn761 <dltn761@gmail.com>
2025-05-22 20:26:55 -0700

    creating desktop dashboard

---
+++ days_remaining.py
@@ -0,0 +1,34 @@
+import time
+import datetime
+
+today = datetime.date.today()
+last_day = datetime.date(2026, 4, 30)
+skillbridge_day = datetime.date(2026, 1, 1)
+
+
+def days_left():
+    days_remaining = (last_day - today).days
+    if days_remaining <= 0:
+        return "It's finally here!"
+    else:
+        return f"{days_remaining} days left until you're out of the Navy!"
+
+def skillbridge_days_left():
+    skillbridge_days_remaining = (skillbridge_day - today).days
+    if skillbridge_days_remaining <= 0:
+        return "Time to be an intern!"
+    else:
+        return f"{skillbridge_days_remaining} until you become an intern!"
+
+def question():
+    requested = input("What would you like to know today? ")
+    if requested == "days left":
+        print(days_left())
+    elif requested == "skillbridge":
+        print(skillbridge_days_left())
+    else:
+        print("Please choose a correct input")
+        
+
+if __name__ == "__main__":
+    question()
